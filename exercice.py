994a938ecff10b00d89c48cea96d12f6c5c99f95
Imed <imed.benamor.pro@gmail.com>
2025-09-18 10:50:42 +0200

    first push

---
+++ exercice.py
@@ -0,0 +1,17 @@
+# Ici je créer une variable nombre qui a pour valeur le nombre entier 50
+nombre = 100
+# Ci dessous j'affiche la vakleur de la variable nombre
+print(nombre)
+
+
+# Ici je créer une variable nombre2 qui a pour valeur le nombre entier 70
+nombre2 = 70
+
+print(nombre2)
+
+# Ici je veux créer une variable resultat qui va stocker le resultat de l'addition entre nombre et nombre2
+
+resultat = nombre + nombre2
+
+print(resultat)
+
---
+++ soustraction.py
@@ -0,0 +1,5 @@
+nb3 = 100
+nb4 = 20
+
+resultat2 = nb3 - nb4
+print(resultat2)
